49f94810c79c9c43a847256a196b24a27d0ced0d
Olha Bahaieva <bahaieva.olha@gmail.com>
2023-07-05 09:43:20 +0300

    App.jcx is replaced with hooks instead of components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import Searchbar from './Searchbar';
 import ImageGallery from './ImageGallery';
 import getImages from './api/getImages';
@@ -6,97 +6,80 @@ import Button from './Button/Button';
 import Loader from './Loader/Loader';
 import Modal from './Modal/Modal';
 
-class App extends Component {
-  state = {
-    images: [],
-    page: 1,
-    isLoading: false,
-    query: '',
-    largeImageURL: null,
+const App = ({ query, images, page, isLoading, largeImageURL, tags }) => {
+  const [imagesState, setImages] = useState([]);
+  const [pageState, setPage] = useState(1);
+  // eslint-disable-next-line
+  const [isLoadingState, setIsLoading] = useState(false);
+  const [queryState, setQuery] = useState('');
+  const [largeImageURLState, setLargeImageURL] = useState(null);
+
+
+
+  const onSubmit = async query => {
+    setQuery(query);
+    setImages([]);
+    setPage(1);
   };
 
-  fetchPhotoByName = async query => {
-    try {
-      this.setState({ isLoading: true });
-      const photoByName = await getImages(query, this.state.page);
-      this.setState(prevState => ({
-        images: [...prevState.images, ...photoByName.hits],
-      }));
-    } catch (err) {
-      console.log('error', err.message);
-    } finally {
-      this.setState({ isLoading: false });
-    }
+  useEffect(() => {
+    const fetchPhotoByName = async () => {
+      if (queryState !== '' && pageState === 1) {
+        try {
+          setIsLoading(true);
+          const photoByName = await getImages(queryState, pageState);
+          setImages(photoByName.hits);
+        } catch (err) {
+          console.log('error', err.message);
+        } finally {
+          setIsLoading(false);
+        }
+      }
+    };
+  
+    fetchPhotoByName();
+  }, [queryState, pageState]);
+
+  const onLoadMore = async () => {
+    setPage(page + 1);
   };
 
-  onSubmit = async query => {
-    this.setState({ query: query, images: [], page: 1 });
+  const openModal = largeImageURL => {
+    setLargeImageURL(largeImageURL);
   };
 
-  componentDidUpdate(_, prevState) {
-    if (
-      prevState.query !== this.state.query ||
-      prevState.page !== this.state.page
-    ) {
-      this.fetchPhotoByName(this.state.query);
-    }
-  }
-
-  onLoadMore = async () => {
-    this.setState({ 
-      page: this.state.page + 1, 
-    });
+  const closeModal = () => {
+    setLargeImageURL(null);
   };
 
-  openModal = largeImageURL => {
-    this.setState({
-      largeImageURL: largeImageURL,
-    });
-  };
-
-  closeModal = () => {
-    this.setState({ largeImageURL: null });
-  };
-
-  render() {
-    const { images, page, isLoading, largeImageURL, tags } = this.state;
-
-    return (
-      <div
-        style={{
-          height: '100vh',
-          display: 'flex',
-          flexDirection: 'column',
-          fontSize: 40,
-          color: '#010101',
-        }}
-      >
-        <Searchbar onSubmit={this.onSubmit} />
-        {isLoading ? (
-          <Loader />
-        ) : (
-          <>
-            <ImageGallery
-              images={images}
-              page={page}
-              openModal={this.openModal}
-            />
-            {images.length > 0 && images.length >= 12 && (
-              <Button onLoadMore={this.onLoadMore}/>
-            )}
-            
-          </>
-        )}
-        {largeImageURL !== null && (
-          <Modal
-            largeImageURL={largeImageURL}
-            tags={tags}
-            closeModal={this.closeModal}
-          />
-        )}
-      </div>
-    );
-  }
-}
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        fontSize: 40,
+        color: '#010101',
+      }}
+    >
+      <Searchbar onSubmit={onSubmit} />
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <>
+          <ImageGallery images={imagesState} page={pageState} openModal={openModal} />
+          {imagesState.length > 0 && imagesState.length >= 12 && (<Button onLoadMore={onLoadMore} />)}
+        </>
+      )}
+      {largeImageURLState !== null && (
+        <Modal
+          largeImageURL={largeImageURLState}
+          tags={tags}
+          closeModal={closeModal}
+        />
+      )}
+    </div>
+  );
+};
 
 export default App;
